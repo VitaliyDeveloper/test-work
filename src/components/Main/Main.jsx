ccce9464573e6fcb415cf16394e54c8a32db622c
Vitaliy Gerassimov <102645214+VitaliyDeveloper@users.noreply.github.com>
2023-01-19 17:39:34 +0600

    active card

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,76 +1,131 @@
+import { useState } from 'react';
 import ImageCat from '../../img/funbox_logo.png';
+import styles from './Main.module.css';
+
 import {
-  MainSection,
+  //   MainSection,
   Title,
   ListStyle,
   ItemStyle,
   ContentContainer,
+  InfoContainer,
   Text,
+  NameProduct,
+  NameProductSub,
+  TextInfo,
   WeightContainer,
   WeightText,
   WeightTextSub,
   ImgCat,
   TextLinkForBy,
+  LinkForBy,
+  Bolder,
 } from './Main.styled';
 
 const Main = () => {
+  const [isActive, setIsActive] = useState(false);
+
+  const handleMouseLeave = e => {
+    // console.log(e.currentTarget);
+    e.currentTarget.classList.add('hoverBorder');
+  };
+
+  const handleClick = e => {
+    console.log(e.currentTarget);
+    e.currentTarget.classList.remove('hoverBorder');
+    e.currentTarget.classList.toggle('active');
+    setIsActive(!isActive);
+  };
+
   return (
-    <MainSection>
+    <section className={styles.mainSection}>
       <Title>Ты сегодня кормил кота?</Title>
       <ListStyle>
         <ItemStyle>
-          <ContentContainer>
-            <Text>Сказочное заморское яство</Text>
-            <h3>Нямушка</h3>
-            <h4>с фуа-гра</h4>
-            <Text>10 порций мышь в подарок</Text>
-            <WeightContainer>
+          <ContentContainer
+            onMouseLeave={handleMouseLeave}
+            onClick={handleClick}
+          >
+            <InfoContainer>
+              <Text>Сказочное заморское яство</Text>
+              <NameProduct>Нямушка</NameProduct>
+              <NameProductSub>с фуа-гра</NameProductSub>
+              <TextInfo>
+                <Bolder>10</Bolder> порций <br /> мышь в подарок
+              </TextInfo>
+            </InfoContainer>
+            <WeightContainer className="hover">
               <WeightText>
-                0,5 <WeightTextSub>кг</WeightTextSub>{' '}
+                0, 5 <WeightTextSub>кг</WeightTextSub>
+              </WeightText>
+            </WeightContainer>
+            <ImgCat src={ImageCat} alt="cat" width="400" />
+          </ContentContainer>
+          {isActive ? (
+            <TextLinkForBy>Печень утки разварная с артишоками.</TextLinkForBy>
+          ) : (
+            <TextLinkForBy>
+              Чего сидишь? Порадуй котэ,
+              <LinkForBy className="hoverLink">купи.</LinkForBy>
+            </TextLinkForBy>
+          )}
+        </ItemStyle>
+        <ItemStyle>
+          <ContentContainer
+            onMouseLeave={handleMouseLeave}
+            onClick={handleClick}
+          >
+            <InfoContainer>
+              <Text>Сказочное заморское яство</Text>
+              <NameProduct>Нямушка</NameProduct>
+              <NameProductSub>с рыбой</NameProductSub>
+              <TextInfo>
+                <Bolder>40</Bolder> порций <br /> <Bolder>2 </Bolder> мыши в
+                подарок
+              </TextInfo>
+            </InfoContainer>
+            <WeightContainer className="hover">
+              <WeightText>
+                2 <WeightTextSub>кг</WeightTextSub>
               </WeightText>
             </WeightContainer>
             <ImgCat src={ImageCat} alt="cat" width="400" />
           </ContentContainer>
           <TextLinkForBy>
-            Чего сидишь? Порадуй котэ, <span>купи.</span>
+            Чего сидишь? Порадуй котэ,
+            <LinkForBy className="hoverLink">купи.</LinkForBy>
           </TextLinkForBy>
         </ItemStyle>
         <ItemStyle>
-          <ContentContainer>
-            <Text>Сказочное заморское яство</Text>
-            <h3>Нямушка</h3>
-            <h4>с рыбой</h4>
-            <Text>40 порций 2 мыши в подарок</Text>
-            <WeightContainer>
+          <ContentContainer
+            onMouseLeave={handleMouseLeave}
+            onClick={handleClick}
+          >
+            <InfoContainer>
+              <Text>Сказочное заморское яство</Text>
+              <NameProduct>Нямушка</NameProduct>
+              <NameProductSub>с курой</NameProductSub>
+              <TextInfo>
+                <Bolder>100</Bolder> порций <br /> <Bolder>5 </Bolder> мышей в
+                подарок заказчик доволен
+              </TextInfo>
+            </InfoContainer>
+            <WeightContainer className="hover">
               <WeightText>
-                2 <WeightTextSub>кг</WeightTextSub>{' '}
+                5 <WeightTextSub>кг</WeightTextSub>
               </WeightText>
             </WeightContainer>
             <ImgCat src={ImageCat} alt="cat" width="400" />
           </ContentContainer>
           <TextLinkForBy>
-            Чего сидишь? Порадуй котэ, <span>купи.</span>
-          </TextLinkForBy>
-        </ItemStyle>
-        <ItemStyle>
-          <ContentContainer>
-            <Text>Сказочное заморское яство</Text>
-            <h3>Нямушка</h3>
-            <h4>с курой</h4>
-            <Text>100 порций 5 мышей в подарок заказчик доволен</Text>
-            <WeightContainer>
-              <WeightText>
-                5 <WeightTextSub>кг</WeightTextSub>{' '}
-              </WeightText>
-            </WeightContainer>
-            <ImgCat src={ImageCat} alt="cat" width="400" />
-          </ContentContainer>
-          <TextLinkForBy>
-            Чего сидишь? Порадуй котэ, <span>купи.</span>
+            Чего сидишь? Порадуй котэ,
+            <LinkForBy className="hoverLink" onClick={handleClick}>
+              купи.
+            </LinkForBy>
           </TextLinkForBy>
         </ItemStyle>
       </ListStyle>
-    </MainSection>
+    </section>
   );
 };
 
--- src/components/Main/Main.styled.js
+++ src/components/Main/Main.styled.js
@@ -2,7 +2,13 @@ import styled from 'styled-components';
 import bgImage from '../../img/bg.jpg';
 
 export const MainSection = styled.section`
-  background-image: url(${bgImage});
+  background-image: linear-gradient(
+      rgba(0, 0, 0, 0.7),
+      rgba(0, 0, 0, 0),
+      rgba(0, 0, 0, 0),
+      rgba(0, 0, 0, 0.7)
+    ),
+    url(${bgImage});
   background-position: center;
   background-repeat: no-repeat;
   background-size: cover;
@@ -17,10 +23,14 @@ export const MainSection = styled.section`
 `;
 
 export const Title = styled.h2`
+  font-size: 50px;
+  font-weight: lighter;
   text-align: center;
   padding: 20px;
   margin: 0;
   color: #fff;
+  font-family: 'Exo2.0';
+  src: url('./fonts/Exo2.0-Thin.otf');
 `;
 
 export const ListStyle = styled.ul`
@@ -28,26 +38,70 @@ export const ListStyle = styled.ul`
   align-items: center;
   justify-content: center;
   list-style: none;
-  padding: 20px;
+  padding: 0 20px;
 `;
 
 export const ItemStyle = styled.li`
   width: 340px;
   height: 490px;
   margin: 20px;
+  transition: 250ms ease-in-out;
+
+  .active {
+    border-color: var(--second-color);
+    /* background-color: red; */
+    & .hover {
+      background: var(--second-color);
+    }
+  }
+  .hoverBorder {
+    border-color: #2ea8e5;
+    & .hover {
+      background: #2ea8e5;
+    }
+
+    & .hoverLink {
+      text-decoration: underline dashed #2ea8e5;
+      color: #2ea8e5;
+    }
+  }
 `;
 
 export const ContentContainer = styled.div`
   position: relative;
-  background-color: antiquewhite;
+  background-color: #f2f2f2;
   border: 5px solid var(--accent-color);
   border-radius: 10px;
-  height: 450px;
+  height: 500px;
   overflow: hidden;
+  z-index: 2;
+  cursor: pointer;
+`;
+
+export const InfoContainer = styled.div`
+  margin-left: 30px;
 `;
 
 export const Text = styled.p`
   color: var(--grey-color);
+  max-width: 300px;
+`;
+
+export const NameProduct = styled.h3`
+  font-weight: bold;
+  font-size: 47px;
+  margin: 0;
+`;
+
+export const NameProductSub = styled.h3`
+  font-weight: bold;
+  font-size: 23px;
+  margin: 0;
+`;
+
+export const TextInfo = styled.p`
+  color: var(--grey-color);
+  width: 150px;
 `;
 
 export const WeightContainer = styled.div`
@@ -83,9 +137,22 @@ export const WeightTextSub = styled.span`
 export const ImgCat = styled.img`
   position: absolute;
   bottom: -100px;
-  left: -20px;
+  left: -30px;
+  z-index: -1;
 `;
 
 export const TextLinkForBy = styled.p`
+  text-align: center;
   color: #fff;
+`;
+
+export const LinkForBy = styled.span`
+  font-weight: bold;
+  text-decoration: underline dashed var(--accent-color);
+  color: var(--accent-color);
+  cursor: pointer;
+`;
+
+export const Bolder = styled.span`
+  font-weight: bold;
 `;
